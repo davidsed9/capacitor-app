491e009ec58a5b53ec0819714cd1010ccf4eb1d9
davidsed9 <davidsed9@gmail.com>
2023-04-29 18:52:25 +0100

    html viewport changed

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div style={{ margin: "1rem"}}>
-      <h1>David App</h1>
+    <div style={{ margin: "0 1rem 0 1rem", paddingTop: "env(safe-area-inset-top)"}}>
+      <h1>David A</h1>
       <Outlet></Outlet>
     </div>
   );
