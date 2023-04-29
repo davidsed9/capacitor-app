b8f12cf4839d43a7d14edde9ffc2cb0f4a60023d
davidsed9 <davidsed9@gmail.com>
2023-04-29 18:46:01 +0100

    cap config changed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,11 @@ const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'capacitor-app',
   webDir: 'build',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server: {
+    url: 'http://192.168.1.16:3000',
+    cleartext: true
+  }
 };
 
 export default config;
