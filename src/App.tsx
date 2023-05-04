8ddfb8f8ea78536443ba9a25f55989980840dc8a
davidsed9 <davidsed9@gmail.com>
2023-05-04 12:10:25 +0100

    app 'finished'

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import { Outlet } from 'react-router-dom';
-import './App.css';
+
+/* Core Css required for Ionic components  to work properly */
+import '@ionic/react/css/core.css';
+/*Basic Css for apps built with Ionic*/
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css'; //remove if nothing is visible
+import '@ionic/react/css/typography.css';
+/* Optional css utils  that can be  commented out*/
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css'
+import { IonHeader, IonTitle, IonToolbar, setupIonicReact } from '@ionic/react/';
+
+import './theme/variables.css';
+
+setupIonicReact();
 
 function App() {
   return (
-    <div style={{ margin: "0 1rem 0 1rem", paddingTop: "env(safe-area-inset-top)"}}>
-      <h1>David App</h1>
-      <Outlet></Outlet>
-    </div>
+    <>
+    <IonHeader>
+      <IonToolbar color="primary">
+        <IonTitle>Davids Apps</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <Outlet/>
+    </>
   );
 }
 
